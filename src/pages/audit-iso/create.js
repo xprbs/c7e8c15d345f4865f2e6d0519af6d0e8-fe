4aa11ae0d24a3ade3cd8aa0d73d4502824a17ac9
nanangfajaru <nanangfajaru@gmail.com>
2024-01-15 17:22:31 +0700

    create project audit iso

---
+++ src/pages/audit-iso/create.js
@@ -0,0 +1,304 @@
+import React, { useEffect, useState } from 'react'
+
+import {
+  CardHeader,
+  CardContent,
+  InputAdornment,
+  Grid,
+  Card,
+  TextField,
+  Box,
+  Button,
+  Typography,
+  CircularProgress,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormHelperText,
+  Autocomplete
+} from '@mui/material'
+
+// ** Custom Components Imports
+import PageHeader from 'src/@core/components/page-header'
+
+// ** Third Party Imports
+import * as yup from 'yup'
+import { useForm, setValue } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+// ** Imports
+import Icon from 'src/@core/components/icon'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import toast from 'react-hot-toast'
+import { backendApi } from 'src/configs/axios'
+
+const schema = yup.object().shape({
+  audit_name: yup.string().required('Project Name is a required field'),
+  audit_ref: yup.string().required('Document Reference is a required field'),
+  audit_category: yup.string().required('Category is a required field'),
+  audit_location: yup.string().required('Department is a required field'),
+  question_uid: yup.string().required('Question Template is a required field')
+})
+
+function AuditIsoCreate() {
+  const [isDisable, setIsDisable] = useState(false)
+  const [department, setDepartment] = useState([])
+  const [question, setQuestion] = useState([])
+  const [locationId, setLocationId] = useState(null)
+  const [questionId, setQuestionId] = useState(null)
+
+  const [fields, setFields] = useState({
+    audit_name: null,
+    audit_ref: null,
+    audit_category: null,
+    audit_location: locationId,
+    question_uid: questionId
+  })
+
+  const router = useRouter()
+
+  const fieldHandler = e => {
+    const name = e.target.name
+    const value = e.target.value
+
+    setFields({
+      ...fields,
+      [name]: value
+    })
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    mode: 'onBlur',
+    resolver: yupResolver(schema)
+  })
+
+  async function createHandler() {
+    setIsDisable(true)
+
+    const dataForm = JSON.stringify({
+      audit_name: fields.audit_name,
+      audit_ref: fields.audit_ref,
+      audit_category: fields.audit_category,
+      audit_location: fields.audit_location,
+      question_uid: fields.question_uid
+    })
+
+    const myPromise = new Promise((resolve, reject) => {
+      backendApi
+        .post('/audit-checklist/store', dataForm)
+        .then(res => {
+          router.push('/menus')
+          resolve('success')
+        })
+        .catch(error => {
+          console.log(error)
+          reject(error)
+        })
+        .finally(e => setIsDisable(false))
+    })
+
+    toast.promise(myPromise, {
+      loading: 'Loading',
+      success: 'Successfully create data',
+      error: error => {
+        if (error.response.status === 500) return error.response.data.response
+
+        return 'Something error'
+      }
+    })
+  }
+
+  async function getInit() {
+    new Promise((resolve, reject) => {
+      backendApi
+        .post('/web/master/get-dept')
+        .then(res => {
+          setDepartment(res.data.data)
+          resolve('success')
+        })
+        .catch(error => {
+          console.log(error)
+          reject(error)
+        })
+    })
+
+    new Promise((resolve, reject) => {
+      backendApi
+        .post('/web/master/get-question')
+        .then(res => {
+          setQuestion(res.data.data)
+          resolve('success')
+        })
+        .catch(error => {
+          console.log(error)
+          reject(error)
+        })
+    })
+  }
+
+  useEffect(() => {
+    getInit()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <PageHeader title={<Typography variant='h5'>Create Project</Typography>} subtitle={null} />
+      </Grid>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title={null} />
+          <CardContent>
+            <form onSubmit={handleSubmit(createHandler)}>
+              <Grid container spacing={8}>
+                <Grid container item md={6} xs={12} rowSpacing={8}>
+                  <Grid item xs={12}>
+                    <TextField
+                      {...register('audit_name')}
+                      onChange={fieldHandler}
+                      fullWidth
+                      name='audit_name'
+                      label='Project Name'
+                      size='small'
+                      InputLabelProps={{ shrink: true }}
+                      error={Boolean(errors.audit_name)}
+                      helperText={errors.audit_name && errors.audit_name.message}
+                    />
+                  </Grid>
+                  <Grid container item spacing={6}>
+                    <Grid item xs={6}>
+                      <TextField
+                        {...register('audit_category')}
+                        onChange={fieldHandler}
+                        fullWidth
+                        name='audit_category'
+                        label='Category'
+                        size='small'
+                        InputLabelProps={{ shrink: true }}
+                        error={Boolean(errors.audit_category)}
+                        helperText={errors.audit_category && errors.audit_category.message}
+                        defaultValue={'ISO'}
+                        InputProps={{ readOnly: true }}
+                      />
+                    </Grid>
+                    <Grid item xs={6}>
+                      <FormControl fullWidth>
+                        <InputLabel shrink error={Boolean(errors.audit_ref)}>
+                          Document Refrence
+                        </InputLabel>
+                        <Select
+                          {...register('audit_ref')}
+                          label='Document Refrence'
+                          defaultValue=''
+                          notched
+                          name='audit_ref'
+                          size='small'
+                          onChange={fieldHandler}
+                          error={Boolean(errors.audit_ref)}
+                        >
+                          <MenuItem value={'14001'}>ISO 14001</MenuItem>
+                          <MenuItem value={'45001'}>ISO 45001</MenuItem>
+                          <MenuItem value={'22301'}>ISO 22301</MenuItem>
+                        </Select>
+                        {errors.audit_ref && (
+                          <FormHelperText sx={{ color: 'error.main' }}>{errors.audit_ref.message}</FormHelperText>
+                        )}
+                      </FormControl>
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Grid container item md={6} xs={12} rowSpacing={8}>
+                  <Grid item xs={12}>
+                    <FormControl fullWidth>
+                      <Autocomplete
+                        size='small'
+                        options={department}
+                        fullWidth
+                        renderInput={params => (
+                          <TextField
+                            {...params}
+                            {...register('audit_location')}
+                            label='Department'
+                            InputLabelProps={{ shrink: true }}
+                            error={Boolean(errors.audit_location)}
+                          />
+                        )}
+                        onChange={(event, newValue) => {
+                          setLocationId(newValue)
+                        }}
+                        isOptionEqualToValue={(option, value) => option.id === value.id}
+                        value={locationId}
+                      />
+                      {errors.audit_location && (
+                        <FormHelperText sx={{ color: 'error.main' }}>{errors.audit_location.message}</FormHelperText>
+                      )}
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormControl fullWidth>
+                      <Autocomplete
+                        size='small'
+                        options={question}
+                        fullWidth
+                        renderInput={params => (
+                          <TextField
+                            {...params}
+                            {...register('question_uid')}
+                            label='Question Template'
+                            InputLabelProps={{ shrink: true }}
+                            error={Boolean(errors.question_uid)}
+                          />
+                        )}
+                        onChange={(event, newValue) => {
+                          setQuestionId(newValue)
+                        }}
+                        isOptionEqualToValue={(option, value) => option.id === value.id}
+                        value={questionId}
+                      />
+                      {errors.question_uid && (
+                        <FormHelperText sx={{ color: 'error.main' }}>{errors.question_uid.message}</FormHelperText>
+                      )}
+                    </FormControl>
+                  </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                  <Box
+                    sx={{
+                      gap: 5,
+                      display: 'flex',
+                      flexWrap: 'wrap',
+                      alignItems: 'center',
+                      justifyContent: 'left'
+                    }}
+                  >
+                    <Button component={Link} href={'/audit-iso'} variant='outlined' size='small'>
+                      Back
+                    </Button>
+                    <Button type='submit' variant='contained' size='small' disabled={isDisable}>
+                      Save
+                      {isDisable && <CircularProgress size={24} sx={{ position: 'absolute' }} />}
+                    </Button>
+                  </Box>
+                </Grid>
+              </Grid>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  )
+}
+
+AuditIsoCreate.acl = {
+  action: 'manage',
+  subject: 'audit-iso-create'
+}
+
+export default AuditIsoCreate
---
+++ src/pages/audit-iso/index.js
@@ -0,0 +1,329 @@
+// ** React Imports
+import { useState, useEffect, useCallback } from 'react'
+
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
+import { DataGrid } from '@mui/x-data-grid'
+import TextField from '@mui/material/TextField'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  TableBody,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  CircularProgress
+} from '@mui/material'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+// ** Custom Components Imports
+import PageHeader from 'src/@core/components/page-header'
+import Link from 'next/link'
+
+import { useAuth } from 'src/hooks/useAuth'
+import toast from 'react-hot-toast'
+import { backendApi } from 'src/configs/axios'
+
+function AuditIsoPage() {
+  // ** State
+  const [data, setData] = useState({
+    loading: true,
+    rows: [],
+    rowCount: 0,
+    rowsPerPageOptions: [10, 50, 100],
+    pageSize: 10,
+    page: 1,
+    sort: null,
+    filterData: null,
+    reload: false
+  })
+
+  const [openDialogAdd, setOpenDialogAdd] = useState(false)
+  const [dataDelete, setDataDelete] = useState({})
+  const [isLoadingDelete, setIsLoadingDelete] = useState(false)
+
+  const auth = useAuth()
+
+  const updateData = (k, v) => setData(prev => ({ ...prev, [k]: v }))
+
+  const handleDialogToggleDelete = data => {
+    setDataDelete(data)
+    setIsLoadingDelete(false)
+    setOpenDialogAdd(!openDialogAdd)
+  }
+
+  const handleDialogToggleDeleteClose = () => {
+    setIsLoadingDelete(false)
+    setOpenDialogAdd(!openDialogAdd)
+  }
+
+  const handleFilter = useCallback(val => {
+    updateData('filterData', val)
+    updateData('page', 1)
+  }, [])
+
+  const deleteHandler = e => {
+    e.preventDefault()
+    setIsLoadingDelete(true)
+
+    const dataForm = JSON.stringify({
+      row_id: dataDelete.acl_subject
+    })
+
+    const myPromise = new Promise((resolve, reject) => {
+      backendApi
+        .post('/audit-checklist/delete', dataForm)
+        .then(res => {
+          resolve('success')
+          handleDialogToggleDeleteClose()
+          updateData('reload', !data.reload)
+        })
+        .catch(error => {
+          reject(error)
+          handleDialogToggleDeleteClose()
+        })
+    })
+
+    toast.promise(myPromise, {
+      loading: 'Loading',
+      success: 'Successfully delete data',
+      error: error => {
+        if (error.response.status === 500) return error.response.data.response
+
+        return 'Something error'
+      }
+    })
+  }
+
+  useEffect(() => {
+    const initData = async () => {
+      updateData('loading', true)
+
+      const dataForm = JSON.stringify({
+        limit: data.pageSize,
+        page: data.page,
+        sort: data.sort,
+        filterData: data.filterData
+      })
+
+      await backendApi
+        .post('/audit-checklist/list', dataForm)
+        .then(response => {
+          updateData('rowCount', response.data.total)
+          setTimeout(() => {
+            updateData('rows', response.data.data)
+            updateData('loading', false)
+          }, 100)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+        .finally(e => updateData('loading', false))
+    }
+
+    initData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data.page, data.pageSize, data.sort, data.filterData, data.reload])
+
+  const columns = [
+    {
+      field: 'id',
+      flex: 0.25,
+      minWidth: 75,
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: row => row.id,
+      renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>#</Typography>
+    },
+    {
+      flex: 1,
+      minWidth: 100,
+      field: 'dataAreaId',
+      renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>Company</Typography>
+    },
+    {
+      flex: 1,
+      minWidth: 100,
+      field: 'audit_date',
+      renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>Date</Typography>
+    },
+    {
+      flex: 1,
+      minWidth: 100,
+      field: 'audit_type',
+      renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>Type</Typography>
+    },
+    {
+      flex: 1,
+      minWidth: 200,
+      field: 'audit_number',
+      renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>Number</Typography>
+    },
+    {
+      flex: 1,
+      minWidth: 200,
+      field: 'audit_name',
+      renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>Doc Name</Typography>
+    },
+    {
+      flex: 1,
+      minWidth: 200,
+      field: 'audit_location',
+      renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>Location</Typography>
+    },
+    {
+      flex: 0.15,
+      minWidth: 115,
+      sortable: false,
+      filterable: false,
+      field: 'actions',
+      disableColumnMenu: true,
+      renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>Actions</Typography>,
+      renderCell: ({ row }) => (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton>
+            <Icon icon='mdi:pencil-outline' />
+          </IconButton>
+          <IconButton onClick={() => handleDialogToggleDelete(row)}>
+            <Icon icon='mdi:delete-outline' />
+          </IconButton>
+        </Box>
+      )
+    }
+  ]
+
+  return (
+    <>
+      <Grid container spacing={6}>
+        <Grid item xs={12}>
+          <PageHeader title={<Typography variant='h5'>Audit ISO</Typography>} subtitle={null} />
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <Box
+              sx={{
+                p: 5,
+                pb: 3,
+                display: 'flex',
+                flexWrap: 'wrap',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+              }}
+            >
+              <Button sx={{ mb: 2.5 }} component={Link} variant='contained' href='/audit-iso/create' size='small'>
+                Create Project
+              </Button>
+              <TextField
+                type={'search'}
+                size='small'
+                sx={{ mr: 4, mb: 2.5 }}
+                placeholder='Search'
+                onChange={e => handleFilter(e.target.value)}
+              />
+            </Box>
+            <Box
+              sx={{
+                px: 5
+              }}
+            >
+              <DataGrid
+                autoHeight
+                rowHeight={40}
+                columns={columns}
+                disableSelectionOnClick
+                disableColumnFilter
+                disableColumnSelector
+                getRowId={row => row.id}
+                loading={data.loading}
+                rows={data.rows}
+                pageSize={data.pageSize}
+                rowsPerPageOptions={data.rowsPerPageOptions}
+                pagination
+                page={data.page - 1}
+                rowCount={data.rowCount}
+                paginationMode='server'
+                onPageChange={newPage => {
+                  updateData('page', newPage + 1)
+                }}
+                onPageSizeChange={newPageSize => {
+                  updateData('page', 1)
+                  updateData('pageSize', newPageSize)
+                }}
+                sortingMode='server'
+                onSortModelChange={newSort => {
+                  updateData('page', 1)
+                  updateData('sort', newSort)
+                }}
+
+                // sx={{ '& .MuiDataGrid-columnHeaders': { borderRadius: 0 } }}
+              />
+            </Box>
+          </Card>
+        </Grid>
+      </Grid>
+
+      <Dialog fullWidth onClose={handleDialogToggleDeleteClose} open={openDialogAdd}>
+        <DialogTitle sx={{ pt: 6, mx: 'auto', textAlign: 'center' }}>
+          <Typography variant='h5' component='span' sx={{ mb: 2 }}>
+            Are you sure to delete data ?
+          </Typography>
+          <Typography variant='body2'>After you delete, you can not undo this data.</Typography>
+        </DialogTitle>
+        <DialogContent sx={{ pb: 6, mx: 'auto' }}>
+          <form onSubmit={deleteHandler}>
+            <TableContainer sx={{ mb: 6 }}>
+              <Table size='small'>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Menu Type</TableCell>
+                    <TableCell>Menu Name</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow key={dataDelete.row_id}>
+                    <TableCell>{dataDelete.menus_type}</TableCell>
+                    <TableCell>{dataDelete.menus_name}</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <Box className='demo-space-x' sx={{ '& > :last-child': { mr: '0 !important', mt: 2 } }}>
+              <Button size='large' type='submit' variant='contained' color='error' disabled={isLoadingDelete}>
+                Delete
+                {isLoadingDelete && <CircularProgress size={24} sx={{ position: 'absolute' }} />}
+              </Button>
+              <Button
+                type='reset'
+                size='large'
+                variant='outlined'
+                color='secondary'
+                onClick={handleDialogToggleDeleteClose}
+              >
+                Cancel
+              </Button>
+            </Box>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </>
+  )
+}
+
+AuditIsoPage.acl = {
+  action: 'manage',
+  subject: 'iso'
+}
+
+export default AuditIsoPage
