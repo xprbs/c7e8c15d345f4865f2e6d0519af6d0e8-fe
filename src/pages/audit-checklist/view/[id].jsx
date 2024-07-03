9fdc85deecd27cec654381b8ae8b2bb89b23272d
Aswardi Azis <aswardiazis@gmail.com>
2024-07-03 16:10:32 +0700

    ballon massage

--- src/pages/audit-checklist/view/[id].jsx
+++ src/pages/audit-checklist/view/[id].jsx
@@ -15,11 +15,16 @@ import toast from 'react-hot-toast'
 import { useRouter } from 'next/router'
 import { backendApi } from 'src/configs/axios'
 import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Badge,
   Box,
   Button,
   Card,
   CardContent,
-  CardHeader,
+  // CardHeader,
   CircularProgress,
   FormControlLabel,
   Grid,
@@ -34,8 +39,20 @@ import {
   TableHead,
   TableRow,
   TextField,
-  Typography
+  Typography,
+  FormControl
 } from '@mui/material'
+
+import { styled } from '@mui/material/styles'
+import TimelineDot from '@mui/lab/TimelineDot'
+import TimelineItem from '@mui/lab/TimelineItem'
+import CardHeader from '@mui/material/CardHeader'
+import TimelineContent from '@mui/lab/TimelineContent'
+import TimelineSeparator from '@mui/lab/TimelineSeparator'
+import TimelineConnector from '@mui/lab/TimelineConnector'
+import MuiTimeline from '@mui/lab/Timeline'
+import dynamic from 'next/dynamic'
+
 import AuditInfo from 'src/views/pages/audit/AuditInfo'
 import AuditorAuditee from 'src/views/pages/audit/AuditorAuditee'
 import ApprovalList from 'src/views/pages/audit/ApprovalList'
@@ -56,6 +73,13 @@ const AuditIsoViewPage = () => {
   const [questionDetail, setQuestionDetail] = useState([])
   const [auditAnswer, setAuditAnswer] = useState([])
   const [selectedDetail, setSelectedDetail] = useState([])
+  const [questionDetailId, setQuestionDetailId] = useState(null)
+  const [auditId, setAuditId] = useState(null)
+  const [questionId, setQuestionId] = useState(null)
+  const [openModal, setOpenModal] = useState(false)
+  const [note, setNote] = useState([])
+  const [isNoteLoading, setIsNoteLoading] = useState(false)
+  const [noteDescription, setNoteDescription] = useState('')
 
   // console.log(auditAnswer)
   // console.log(selectedDetail)
@@ -74,6 +98,8 @@ const AuditIsoViewPage = () => {
         .then(res => {
           resolve('success')
           setDetail(res.data.data)
+          setQuestionId(res.data.data.question_uid)
+          setAuditId(res.data.data.audit_uid)
           getQuestionDetail(res.data.data.question_uid, res.data.data.audit_uid)
           setTimeout(() => {
             getAuditAnswer(res.data.data.audit_uid, res.data.data.question_uid)
@@ -133,8 +159,62 @@ const AuditIsoViewPage = () => {
     })
   }
 
-  const createHandler = async is_submit => {
+  const Timeline = styled(MuiTimeline)({
+    paddingLeft: 0,
+    paddingRight: 0,
+    '& .MuiTimelineItem-root': {
+      width: '100%',
+      '&:before': {
+        display: 'none'
+      }
+    }
+  })
+
+  var Editor = dynamic(() => import('src/views/editor/cke-editor'), {
+    ssr: false
+  })
+
+  const handleClickOpenModal = (audit_uid, question_uid, question_detail_uid) => {
+    setOpenModal(true)
+    setQuestionDetailId(question_detail_uid)
+    getNote(audit_uid, question_uid, question_detail_uid)
+  }
+
+  const getNote = (audit_uid, question_uid, question_detail_uid) => {
+    setIsNoteLoading(true)
+    new Promise((resolve, reject) => {
+      backendApi
+        .post(
+          '/web/approval/approval-note-get',
+          JSON.stringify({
+            audit_uid: audit_uid ?? null,
+            question_uid: question_uid ?? null,
+            question_detail_uid: question_detail_uid ?? null
+          })
+        )
+        .then(res => {
+          setNote(res.data.data)
+          resolve('success')
+        })
+        .catch(error => {
+          console.log(error)
+          reject(error)
+        })
+        .finally(e => setIsNoteLoading(false))
+    })
+  }
+
+  const handleCloseModal = () => setOpenModal(false)
+
+  const createHandler = async (audit_uid, question_uid, question_detail_uid, is_submit) => {
     setIsDisable(true)
+
+    const dataForm = JSON.stringify({
+      audit_uid: audit_uid,
+      question_uid: question_uid,
+      question_detail_uid: question_detail_uid,
+      note: noteDescription
+    })
 
     const formData = new FormData()
 
@@ -163,6 +243,36 @@ const AuditIsoViewPage = () => {
 
     // formData.append('files', imgsSrc)
 
+    // const myPromise = new Promise((resolve, reject) => {
+    //   backendApi
+    //     .post('/web/audit-checklist/answer-store', formData, {
+    //       headers: {
+    //         'Content-Type': 'multipart/form-data'
+    //       }
+    //     })
+    //     .then(res => {
+    //       if (is_submit === 1) {
+    //         router.push('/audit-checklist')
+    //       }
+    //       getQuestionDetail(detail.question_uid, detail.audit_uid)
+    //       clearUploadFile()
+    //       resolve('success')
+    //     })
+
+    //     .post('/web/approval/approval-note-store', dataForm)
+    //     .then(res => {
+    //       resolve('success')
+    //     })
+    //     .catch(error => {
+    //       console.log(error)
+    //       reject(error)
+    //     })
+    //     .finally(e => {
+    //       setNoteDescription('')
+    //       setIsDisable(false)
+    //     })
+    // })
+
     const myPromise = new Promise((resolve, reject) => {
       backendApi
         .post('/web/audit-checklist/answer-store', formData, {
@@ -176,13 +286,17 @@ const AuditIsoViewPage = () => {
           }
           getQuestionDetail(detail.question_uid, detail.audit_uid)
           clearUploadFile()
+          return backendApi.post('/web/approval/approval-note-store', dataForm)
+        })
+        .then(res => {
           resolve('success')
         })
         .catch(error => {
           console.log(error)
           reject(error)
         })
-        .finally(e => {
+        .finally(() => {
+          setNoteDescription('')
           setIsDisable(false)
         })
     })
@@ -428,6 +542,18 @@ const AuditIsoViewPage = () => {
                                             multiple
                                           />
                                         </Button>
+                                        <Grid sx={{ p: 2, py: 3 }}>
+                                          <Badge
+                                            badgeContent={data.count_note}
+                                            color={'error'}
+                                            onClick={() =>
+                                              handleClickOpenModal(auditId, questionId, data.question_detail_uid)
+                                            }
+                                            sx={{ cursor: 'pointer' }}
+                                          >
+                                            <Icon icon='tabler:message-dots' />
+                                          </Badge>
+                                        </Grid>
                                         <Table cellpadding='3'>
                                           {data.files &&
                                             data.files.map((d, i) => (
@@ -493,7 +619,101 @@ const AuditIsoViewPage = () => {
                       </Table>
                     </TableContainer>
                   </Grid>
-                  <Grid item>
+                  <Dialog
+                    open={openModal}
+                    onClose={handleCloseModal}
+                    aria-labelledby='form-dialog-title'
+                    scroll={'paper'}
+                    fullWidth={true}
+                    maxWidth={'lg'}
+                  >
+                    <DialogTitle id='scroll-dialog-title'>Note History</DialogTitle>
+                    {isNoteLoading ? (
+                      <Grid sx={{ p: 4 }}>
+                        <Skeleton variant='text' sx={{ fontSize: '1rem' }} />
+                        <Skeleton variant='text' sx={{ fontSize: '3rem' }} />
+                      </Grid>
+                    ) : (
+                      <DialogContent dividers={scroll === 'paper'}>
+                        {note.length ? (
+                          note.map((data, index) => (
+                            <Timeline key={index} sx={{ my: 0, py: 0 }}>
+                              <TimelineItem>
+                                <TimelineSeparator>
+                                  <TimelineDot color='warning' />
+                                  <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent sx={{ mt: 0, mb: theme => `${theme.spacing(2)} !important` }}>
+                                  <Box
+                                    sx={{
+                                      display: 'flex',
+                                      flexWrap: 'wrap',
+                                      alignItems: 'center',
+                                      justifyContent: 'space-between'
+                                    }}
+                                  >
+                                    <Typography sx={{ mr: 2, fontWeight: 500 }}>{data.created_by}</Typography>
+                                    <Typography variant='body2' sx={{ color: 'text.disabled' }}>
+                                      {data.created_at}
+                                    </Typography>
+                                  </Box>
+                                  <Typography sx={{ mb: 2, color: 'text.secondary' }}>
+                                    <a dangerouslySetInnerHTML={{ __html: data.note }}></a>
+                                  </Typography>
+                                </TimelineContent>
+                              </TimelineItem>
+                            </Timeline>
+                          ))
+                        ) : (
+                          <Typography variant='subtitle2' sx={{ display: 'flex', p: 2 }}>
+                            No data found
+                          </Typography>
+                        )}
+                      </DialogContent>
+                    )}
+                    <FormControl fullWidth>
+                      <TextField
+                        name={'note_description'}
+                        value={noteDescription}
+                        multiline
+                        rows={3}
+                        fullWidth
+                        label='Note'
+                        size='small'
+                        InputLabelProps={{ shrink: true }}
+                        sx={{ minWidth: 320, mt: 2 }}
+                        onChange={e => {
+                          // console.log(e.target.value)
+                          setNoteDescription(e.target.value)
+                        }}
+                      />
+                    </FormControl>
+                    <Grid item sx={{ mt: 4, mb: 4, ml: 4 }}>
+                      <Box
+                        sx={{
+                          gap: 5,
+                          display: 'flex',
+                          flexWrap: 'wrap',
+                          alignItems: 'center',
+                          justifyContent: 'left'
+                        }}
+                      >
+                        <Button
+                          onClick={e => createHandler(auditId, questionId, questionDetailId)}
+                          variant='contained'
+                          size='small'
+                          disabled={isDisable}
+                        >
+                          Save
+                          {isDisable && <CircularProgress size={24} sx={{ position: 'absolute' }} />}
+                        </Button>
+                        <Button size='small' onClick={handleCloseModal}>
+                          Cancel
+                        </Button>
+                      </Box>
+                    </Grid>
+                  </Dialog>
+                  <Grid item sx={{ mt: 4 }}>
                     <Box
                       sx={{
                         gap: 5,
@@ -506,11 +726,21 @@ const AuditIsoViewPage = () => {
                       <Button component={Link} href={'/audit-checklist'} variant='outlined' size='small'>
                         Back
                       </Button>
-                      <Button onClick={e => createHandler(0)} variant='contained' size='small' disabled={isDisable}>
+                      <Button
+                        onClick={e => createHandler(0, auditId, questionId, questionDetailId)}
+                        variant='contained'
+                        size='small'
+                        disabled={isDisable}
+                      >
                         Save as Draft
                         {isDisable && <CircularProgress size={24} sx={{ position: 'absolute' }} />}
                       </Button>
-                      <Button onClick={e => createHandler(1)} variant='contained' size='small' disabled={isDisable}>
+                      <Button
+                        onClick={e => createHandler(1, auditId, questionId, questionDetailId)}
+                        variant='contained'
+                        size='small'
+                        disabled={isDisable}
+                      >
                         Submit for approval
                         {isDisable && <CircularProgress size={24} sx={{ position: 'absolute' }} />}
                       </Button>
